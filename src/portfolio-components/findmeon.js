f2048441a25d8f70d63101cde139c3084f0f7646
Roshan Khadka <roshan.khadka.u@gmail.com>
2022-08-29 01:17:22 +1000

    contact links updated

--- src/portfolio-components/findmeon.js
+++ src/portfolio-components/findmeon.js
@@ -5,9 +5,9 @@ import { faFacebookF, faLinkedinIn, faGithub } from "@fortawesome/free-brands-sv
 const FindMeOn = () =>{
     return(
         <span>
-            <FontAwesomeIcon icon={faFacebookF} style={{padding: "15px", marginRight:"10px", backgroundColor:"black", cursor:"pointer", borderRadius:"5px"}}></FontAwesomeIcon>
-            <FontAwesomeIcon icon={faLinkedinIn} style={{padding: "15px", marginRight:"10px", backgroundColor:"black", cursor:"pointer", borderRadius:"5px"}}></FontAwesomeIcon>
-            <FontAwesomeIcon icon={faGithub} style={{padding: "15px", marginRight:"10px", backgroundColor:"black", cursor:"pointer", borderRadius:"5px"}}></FontAwesomeIcon>            
+            <a href="https://www.linkedin.com/in/roshan-khadka-050b3417b/" rel="noreferrer" target="_blank" style={{color:"white"}}><FontAwesomeIcon icon={faLinkedinIn} style={{padding: "15px", marginRight:"10px", backgroundColor:"black", cursor:"pointer", borderRadius:"5px"}}></FontAwesomeIcon></a>
+            <a href="https://www.facebook.com/whitz.rock" rel="noreferrer" target="_blank" style={{color:"white"}}><FontAwesomeIcon icon={faFacebookF} style={{padding: "15px", marginRight:"10px", backgroundColor:"black", cursor:"pointer", borderRadius:"5px"}}></FontAwesomeIcon></a>
+            <a href="https://github.com/roshan-77" rel="noreferrer" target="_blank" style={{color:"white"}}><FontAwesomeIcon icon={faGithub} style={{padding: "15px", marginRight:"10px", backgroundColor:"black", cursor:"pointer", borderRadius:"5px"}}></FontAwesomeIcon></a>          
         </span>
     )
 }
--- src/portfolio-components/footer.js
+++ src/portfolio-components/footer.js
@@ -9,8 +9,7 @@ const Footer =() =>{
         <div className = "global-container" style={{color:"white", textAlign:"center", padding:"40px"}}>
             <h1 style={{padding:"10px"}}>Roshan</h1>
             <FindMeOn />
-            <FontAwesomeIcon icon={faYoutube} style={{padding: "15px", marginRight:"10px", backgroundColor:"black", cursor:"pointer", borderRadius:"5px"}}></FontAwesomeIcon>
-            <FontAwesomeIcon icon={faInstagram} style={{padding: "15px", marginRight:"10px", backgroundColor:"black", cursor:"pointer", borderRadius:"5px"}}></FontAwesomeIcon>
+            <a href="https://www.youtube.com/channel/UCU6Q97SvCmyMt3FM0CR5fDw" style={{color:"white"}} rel="noreferrer" target="_blank"><FontAwesomeIcon icon={faYoutube} style={{padding: "15px", marginRight:"10px", backgroundColor:"black", cursor:"pointer", borderRadius:"5px"}}></FontAwesomeIcon></a>
             <div style={{padding:"20px 0 0 0"}}>Roshan &copy; All Rights Reserved </div>
         </div>
     )
